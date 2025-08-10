9b0d3370568393414f22e289ef9eebe751b0befd
Marten Richter <marten.richter@freenet.de>
2025-08-10 22:46:39 +0200

    Add propsToHandle as replacement for Emval.toHandle (#8)

--- test/run_test_zstd.sh
+++ test/run_test_zstd.sh
@@ -8,7 +8,7 @@ echo "Check node version..."
 "$NODE" --version
 echo "Building test..."
 emcc wasmfs_squashfs.cc -o wasmfs_squashfs.js -sENVIRONMENT=node -O2 -sWASMFS  --embed-file squashfs_example_zstd.sqshfs \
-                                         -DTEST_COMPRESSIONS_ZSTD --use-port=../ports/libzstd.py \
+                                         -DTEST_COMPRESSIONS_ZSTD --use-port=../ports/libzstd.py -lembind \
                                          -std=c++11 --use-port=../ports/libsquashfs.py:compressions=zstd --use-port=../ports/emscripten_wasm_squashfs.py
 #emcc wasmfs_squashfs.c -o wasmfs_squashfs.js -sENVIRONMENT=node -O2 -sWASMFS --embed-file squashfs_example_zstd.sqshfs \
 #                        -DTEST_COMPRESSIONS_ZSTD --use-port=../ports/libzstd.py \
